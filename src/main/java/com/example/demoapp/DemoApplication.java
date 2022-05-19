ee7d0c6063a9da8ca00192978e4ac7e888490ed1
Timur <t.zhakin@andersenlab.com>
2022-05-19 22:30:28 +0600

    renamed main class from DemoAppApplication.java

--- src/main/java/com/example/demoapp/DemoApplication.java
+++ src/main/java/com/example/demoapp/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demoapp;
 import com.example.demoapp.service.AccountService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class DemoAppApplication {
+public class DemoApplication {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/context.xml");
